f4734626800a180f052393ac809a2f2ed9d31f86
lovenumbers123 <lovenumbers123@yahoo.com>
2016-12-11 16:36:11 -0600

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,77 @@
+
+R version 3.3.1 (2016-06-21) -- "Bug in Your Hair"
+Copyright (C) 2016 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[Previously saved workspace restored]
+
+> ## Plot 4 - How have emissions from coal combustion-related sources changed from 1999 - 2008
+> ## downloaded and unzipped necessary file
+>  if(!file.exists("./data")){dir.create("./data")}
+>  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+>  download.file(fileUrl,destfile="./data/Pollution.zip",method="auto")
+trying URL 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip'
+Content type 'application/zip' length 30643310 bytes (29.2 MB)
+downloaded 29.2 MB
+
+>  unzip(zipfile="./data/Pollution.zip",exdir="./data")
+> 
+> ## Set working Directory
+> setwd("~/data/")
+> 
+> ## Read Data
+> NEI <- readRDS("summarySCC_PM25.rds")
+> SCC <- readRDS("Source_Classification_Code.rds")
+> 
+> ## Extract Year
+> Emissions <- summary(NEI$Emissions, year)
+> 
+> ## Load Library - DPLYR
+> library(dplyr)
+
+Attaching package: ‘dplyr’
+
+The following objects are masked from ‘package:stats’:
+
+    filter, lag
+
+The following objects are masked from ‘package:base’:
+
+    intersect, setdiff, setequal, union
+
+> 
+> ## Load Library - ggplot2
+> library(ggplot2)
+Warning message:
+package ‘ggplot2’ was built under R version 3.3.2 
+> 
+> cc <- grepl("Fuel Comb.*Coal", SCC$EI.Sector)
+> 
+> ccs <- SCC[cc,]
+> 
+> ccemissions <- NEI[(NEI$SCC %in% ccs$SCC), ]
+> 
+> coalemissions <- summarise(group_by(ccemissions, year), Emissions=sum(Emissions))
+> 
+> png('plot4.png')
+> 
+> ggplot(coalemissions, aes(x=factor(year), y=Emissions/1000,fill=year)) + geom_bar(stat="identity") + xlab("Year") + ylab("Total Emissions")  + ggtitle("Coal Combustion-Related Sources Emissions") 
+> 
+> dev.off()
+null device 
+          1 
+> 
